66b75c524791fe231e36e7d6a7c8a0ef47eb8047
nileshchouksey <nileshchouksey9179@gmail.com>
2025-06-28 20:32:26 +0530

    background change

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import BG from "../assets/BG.png";
+
 export const Hero = () => {
   return (
     <>
       <div className="-mt-30 relative h-200 flex justify-center items-center">
         <img src={BG} alt="" className="absolute -z-1" />\
-        <div className="grid justify-center items-center top-10">
-          <h1 className="text-red-500 text-8xl font-extrabold font-[family-name:var(--customfont)] ">
-            Turn Your Dreams <br />
-            Into Reality
-          </h1>
+        <div className="grid justify-center items-center top-15">
+          <h1 class="fw-bold display-1 text-white aos-init aos-animate" data-aos="fade-up" data-aos-duration="500"  >Turning Dreams into Reality</h1>
 
           <div className="flex justify-center items-center gap-10 ">
             <button className="rounded px-5 py-3 bg-red-400 ">Log In</button>
